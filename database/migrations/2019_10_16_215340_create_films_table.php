083da75ee86ed64ba51040a7cc6be88954fd811e
mix <mixartemev@gmail.com>
2019-10-22 09:40:27 +0300

    db

---
+++ database/migrations/2019_10_16_215340_create_films_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->decimal('rating', 4, 2);
+            $table->unsignedSmallInteger('duration')->comment('In seconds');
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
